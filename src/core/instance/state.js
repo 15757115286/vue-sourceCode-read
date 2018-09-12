de4030bd4a2e6dd094ffbf4693cc83a43d301319
xuweitao <2270532540@qq.com>
2018-09-12 16:55:32 +0800

    阅读完源码 initState —— initData(observe没阅读)

--- src/core/instance/state.js
+++ src/core/instance/state.js
@@ -37,6 +37,7 @@ const sharedPropertyDefinition = {
   set: noop
 }
 
+// 把target.sourceKey上面的的键值对代理到target上面
 export function proxy (target: Object, sourceKey: string, key: string) {
   sharedPropertyDefinition.get = function proxyGetter () {
     return this[sourceKey][key]
@@ -49,16 +50,35 @@ export function proxy (target: Object, sourceKey: string, key: string) {
 
 // 初始化vm的状态
 export function initState (vm: Component) {
+
+  // 首先往vm实例上面添加_watchers字段，初始值为一个数组
   vm._watchers = []
+
+  // 获取已经经过mergeOptions处理过后的option
   const opts = vm.$options
+
+  // 重点来了，我们在created中使用props的值是在这里加载的
   if (opts.props) initProps(vm, opts.props)
+
+  // 重点来了，我们在created中使用methods的值是在这里加载的
   if (opts.methods) initMethods(vm, opts.methods)
+
+  // 在这里初始化data的数据。因为initProps在initData之前，所以这就是为什么我们
+  // 可以用props的值去初始化data的值的原因
+  // 我们Vue实例中原型上的$data就是代理的vm._data这个值
   if (opts.data) {
+    // 如果在vm.$options上存在data属性，那么使用initData方法去初始化vm的data
     initData(vm)
   } else {
+    // 通过observe函数观测一个空对象，并把vm._data的值指向它
     observe(vm._data = {}, true /* asRootData */)
   }
+
+  // 重点来了，我们在created中使用computed的值是在这里加载的
   if (opts.computed) initComputed(vm, opts.computed)
+
+  // 重点来了，我们在created中使用watch的值是在这里加载的
+  // 这里opts.watch !== nativeWatch是为了兼容Firefox游览器
   if (opts.watch && opts.watch !== nativeWatch) {
     initWatch(vm, opts.watch)
   }
@@ -113,10 +133,20 @@ function initProps (vm: Component, propsOptions: Object) {
 }
 
 function initData (vm: Component) {
+  // 获取合并属性上的data的值
   let data = vm.$options.data
+
+  // 这里一般来说我们在mergeOption的时候已经把$options的处理成为一个函数了
+  // 这里这么做的目的是因为beforeCreated函数是在mergeOptions之后且在initState函数之前
+  // 调用的，因为vm.$options不是只读属性，如果在那个时候对vm.$options的值做出了修改，那么
+  // 这里的判断是必须的
+
+  // 我们通过getData函数拿到了最终的值并且赋值给了vm._data并且重写了data的值，此时data已经不是函数而是一个确切的值
   data = vm._data = typeof data === 'function'
     ? getData(data, vm)
     : data || {}
+
+  // 如果拿到的值不是一个纯对象的话在非生产环境会产生告警，并且把data的值设置为一个空对象
   if (!isPlainObject(data)) {
     data = {}
     process.env.NODE_ENV !== 'production' && warn(
@@ -125,13 +155,26 @@ function initData (vm: Component) {
       vm
     )
   }
-  // proxy data on instance
+
+  // 在vm实例上代理data的值
+
+  // 获取data上的属性名称
   const keys = Object.keys(data)
+
+  // 获取props的属性名称
   const props = vm.$options.props
+
+  // 获取methods的属性名称
   const methods = vm.$options.methods
   let i = keys.length
+
+  // 这里检查data上的属性名称是否和props和methods中的冲突
   while (i--) {
     const key = keys[i]
+
+    // 因为props、data、methods中的所有属性名都会被代理到vm实例上面去，所以这就硬性的规定了这几个对象里面
+    // 不能有重名的属性。优先级是 props > data > methods
+    // 如果一个属性在data中已经定义了，那么这个属性名就不能再methods中再去定义，否则在非生产环境会产生告警
     if (process.env.NODE_ENV !== 'production') {
       if (methods && hasOwn(methods, key)) {
         warn(
@@ -140,6 +183,8 @@ function initData (vm: Component) {
         )
       }
     }
+
+    // 如果一个属性名称已经在props中已经定义了，那么这个值就不能再data中再去使用，否则在非生产环境会产生告警
     if (props && hasOwn(props, key)) {
       process.env.NODE_ENV !== 'production' && warn(
         `The data property "${key}" is already declared as a prop. ` +
@@ -147,17 +192,23 @@ function initData (vm: Component) {
         vm
       )
     } else if (!isReserved(key)) {
+      // 如果不是保留字（其实是符合不是_或者$开头的属性名），那么就把_data上的这个键值对代理到vm上面
+      // 这里也提示了我们一点vue是不会代理_或者$开头的属性
+      // 说白了就可以直接用 this.key 来获取 而不需要用 this._data.key 去获取值
       proxy(vm, `_data`, key)
     }
   }
-  // observe data
+
+  // 把最终的data（vm._data）对象变成一个可观察（响应式）的对象
   observe(data, true /* asRootData */)
 }
 
 export function getData (data: Function, vm: Component): any {
   // #7573 disable dep collection when invoking data getters
+  // 防止使用 props 数据初始化 data 数据时收集冗余的依赖（摘自 —— Vue技术内幕） 
   pushTarget()
   try {
+    // 调用并返回data执行后的值（data是我们在合并options时候生成的mergedInstanceDataFn或者mergedDataFn函数）
     return data.call(vm, vm)
   } catch (e) {
     handleError(e, vm, `data()`)
